1553c485f35420b47bb955bfd6a855496f71829b
celiarediaz-del <celia.r.e.diaz@gmail.com>
2026-03-01 17:22:21 +0100

    Initial Spring Boot project setup

---
+++ gui/src/test/java/com/gui/gui/GuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
